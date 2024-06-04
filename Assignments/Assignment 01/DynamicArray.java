57f9aa572611bf71aebe8a5201868caf33f5b20a
Mikeq21 <132224416+Mikeq21@users.noreply.github.com>
2024-06-04 17:00:37 -0500

    First Attempt - HW 1

--- Assignments/Assignment 01/DynamicArray.java
+++ Assignments/Assignment 01/DynamicArray.java
@@ -1,16 +1,4 @@
 /*
- * In the class below, write two methods:
- * 
- * First, method resize() to expand the existing array data by doubling its size.
- *
- * Second, method contains(String string) that returns true if String string already
- * exists in array data and false otherwise.
- *
- * Your code must have comments explaining what is done and why it is done in the
- * way you do it.
- *
- * DO NOT USE any tools that require to be imported, ie, do not use the import command.
- *
  * To save this assignment, make sure you commit your changes to your GitHub repository,
  * following the instructions in Sakai. IF YOU DO NOT COMMIT THE CHANGES, IT IS POSSIBLE THAT
  * YOUR WORK MAY BE LOST AND YOU MAY HAVE TO START ALL OVER AGAIN.
@@ -55,4 +43,45 @@ public class DynamicArray {
         this.position++;
     } // method add
     
+    public void resize() {
+        //create "newArray" with the size being 10 positions bigger than the original string   
+        String[] newArray= new String[size + 10];
+
+        for(int x; x<data.length; x++){
+            newArray[x] = data[x];
+        }
+    } // method resize
+
+    public boolean contains(String string) {
+        boolean contains = false;
+        //While the array doesn't have the
+        while (!contains){
+            if (data == string){
+                contains = true;
+            }
+        }
+        return contains;
+
+    } // method contains
+
+    public int count(String string) {
+        int count = 0;
+        while(data.length){
+            if (data[data.position] == string){ //If the element of the array is the same as the input string
+                count++;    //Add to the counter
+            }
+            data.position++;    //Add to increase the element position.
+        }
+        return count;
+    } // method count
+
+    public boolean addUnique (String string) {
+        //boolean because we want to know if adding the string we input is a success.
+        boolean success = !this.contains(string);
+        if (sucess){
+            this.add(string);
+        }
+        return success;
+    } // method addUnique
+
 } // class DynamicArray
